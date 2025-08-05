977045456c09b4971d7beab03e5e85981f4cced0
dukerupert <logan@fireflysoftware.dev>
2025-08-05 21:08:46 +0000

    Initial navigation and hero created

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Navigation from '$lib/navigation.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="bg-white">
+	<Navigation />
+	{@render children?.()}
+</div>
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Hero from "$lib/hero.svelte";
+</script>
+
+<Hero />
